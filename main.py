42f2d84fd94d339012111e17b31fe9a05e07cb4f
= <=>
2025-02-01 20:20:44 -0500

    0.1

---
+++ main.py
@@ -0,0 +1,47 @@
+import this
+
+"""
+Beautiful is better than ugly.     
+Explicit is better than implicit.  
+Simple is better than complex.     
+Complex is better than complicated.
+Flat is better than nested.        
+Sparse is better than dense.       
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
+
+"""
+El zen de Python, por Tim Peters
+
+Lo bonito es mejor que lo feo.
+Lo explícito es mejor que lo implícito.
+Lo simple es mejor que lo complejo.
+Lo complejo es mejor que lo complicado.
+Lo plano es mejor que lo anidado.
+Lo disperso es mejor que lo denso.
+La legibilidad cuenta.
+Los casos especiales no son tan especiales como para romper las reglas.
+Aunque la practicidad gana a la pureza.
+Los errores nunca deben pasar en silencio.
+A menos que se silencien explícitamente.
+Ante la ambigüedad, rechaza la tentación de adivinar.
+Debe haber una -y preferiblemente sólo una- forma obvia de hacerlo.
+Aunque esa manera puede no ser obvia al principio, a menos que seas holandés.
+Ahora es mejor que nunca.
+Aunque nunca es mejor que *ahora mismo*.
+Si la implementación es difícil de explicar, es una mala idea.
+Si la implementación es fácil de explicar, puede ser una buena idea.
+Los espacios de nombres son una gran idea, ¡hagamos más de ellos!
+"""
